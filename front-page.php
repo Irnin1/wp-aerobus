37269bca4a61a0395ba46462b4030d0eb9b10cee
Irnin1 <mixail4505@yandex.ru>
2018-08-10 13:43:33 +0500

    tri

--- front-page.php
+++ front-page.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?> 
 
-<div class="container-fluid"></div>
+<div class="container-fluid">
     <div class="row">
         <div class="col-sm-12">
             <div class="slider">
-                <img src="<?php bloginfo('template_url'); ?>/images/slide1.png" width="960" alt="">
+                <img src="<?php bloginfo('template_url'); ?>/images/slider/slide1_min.png" alt="">
             </div>
         </div>
     </div>
 </div>
-
 
 <?php get_footer(); ?>
