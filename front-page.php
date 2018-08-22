c9382b64a3b701c4ba216b8e7242e89505170415
Irnin1 <mixail4505@yandex.ru>
2018-08-22 15:51:27 +0500

    advansed

--- front-page.php
+++ front-page.php
@@ -46,19 +46,81 @@
 
         <div class="col">
             <div class="boxs">
-                <div class="box-1">АРЕНДА ШАТРОВ, МЕБЕЛИ</div> 
+                <div class="box-1">АРЕНДА ШАТРОВ, <br />МЕБЕЛИ</div> 
 
-                <div class="box-2">КЕЙТЕРИНГ</span> 
+                <div class="box-2">КЕЙТЕРИНГ</div> 
 
-                <div class="box-1">ТУРОВЫЕ ПРОЕКТЫ и ПРЕЗЕНТАЦИИ</div> 
+                <div class="box-1">ТУРОВЫЕ ПРОЕКТЫ и <br />ПРЕЗЕНТАЦИИ</div> 
 
-                <div class="box-2">КОРПОРАТИВНЫЕ ПРАЗДНИКИ и СВАДЬБЫ</span> 
+                <div class="box-2">КОРПОРАТИВНЫЕ <br />ПРАЗДНИКИ <br />и СВАДЬБЫ</div> 
 
-                <div class="box-3">КОМПЛЕКС МАРКЕТИНГА и PR</span> 
+                <div class="box-3">КОМПЛЕКС <br />МАРКЕТИНГА и PR</div> 
             </div>
         </div>
 
     </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="none">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="advanteges-item">
+                <div class="item-title"><h2 class="white">ПОЧЕМУ КЛИЕНТЫ ДОВЕРЯЮТ НАМ?</h2></div>
+
+                <div class="advansed-list">
+                    <div class="advanteges-items advantages_items-left">
+                        <div class="advansed-item-menu advansed-item-img1" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/1.png)">
+                            Высокая квалификация</div>
+                        <div class="advansed-item-menu advansed-item-img1" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/2.png)">
+                            Опыт работы более 20 лет</div>
+                        <div class="advansed-item-menu advansed-item-img1" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/3.png)">
+                            Бонусы для постоянных клиентов</div>
+                        <div class="advansed-item-menu advansed-item-img1" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/4.png)">
+                            Высочайший уровень сервиса</div>
+                        <div class="advansed-item-menu advansed-item-img1" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/5.png)">
+                            Уникальные сценарии</div>
+                    </div>
+
+                    <div class="advanteges-items advantages_items-right" >
+                        <div class="advansed-item-menu advansed-item-img2" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/6.png)">
+                            Около 300 мероприятий ежегодно</div>
+                        <div class="advansed-item-menu advansed-item-img2" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/6.png)">
+                            Более 40 федеральных проектов</div>
+                        <div class="advansed-item-menu advansed-item-img2" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/7.png)">
+                            Полный спектр услуг: от  задумки до реализации</div>
+                        <div class="advansed-item-menu advansed-item-img2" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/8.png)">
+                            Передовые методики в области <br />продвижения компании</div>
+                        <div class="advansed-item-menu advansed-item-img2" style="background-image: url(<?php bloginfo('template_url'); ?>/images/items/9.png)">
+                            Собственные производственные мощности: <br />от сцены до звука</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="none">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="question">
+                
+            </div>
+        </div>
+    </div>
+
 </div>
 
 <?php get_footer(); ?>
